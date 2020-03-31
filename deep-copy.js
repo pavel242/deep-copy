6d5ef8e27e90ef47088f499276d78c3b1c87b4fe
pavel242 <pgubarev@gmail.com>
2020-03-31 14:07:59 +0300

    Add files via upload

---
+++ deep-copy.js
@@ -0,0 +1,25 @@
+var deepCopy = (function() {
+	var blob = new Blob(["self.onmessage = function(e) { self.postMessage(e.data); }"], { type: 'application/javascript;' });
+    var url = URL.createObjectURL(blob);
+	var worker = new Worker(url);
+	//URL.revokeObjectURL(url);
+
+	var count = 0;
+	var resolvers = {};
+
+	worker.onmessage = function (event) {
+		resolvers[event.data.id](event.data.source);
+		delete resolvers[event.data.id];
+	};
+
+	function main(data) {
+		var id = ++count;
+
+		return new Promise(function(resolve) {
+            resolvers[id] = resolve;
+			worker.postMessage({source: data, id: id});
+		});
+	}
+
+	return main;
+})();	
